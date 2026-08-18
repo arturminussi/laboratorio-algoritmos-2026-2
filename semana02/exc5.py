9c601dbc930afba068c93662543f2a10b920c5d9
arturminussi <arturminuss7@gmail.com>
2026-08-18 19:36:32 -0300

    Create exc5.py

---
+++ semana02/exc5.py
@@ -0,0 +1,8 @@
+
+carros = int(input("Digite o total de carros: "))
+
+preco_pneu = 395.40
+total_pneus = carros * 4
+valor_final = total_pneus * preco_pneu
+
+print("O valor final será: R$", valor_final)
