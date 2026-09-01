d4b2f59c333ac314a012ae326803791ed35cc3e8
arturminussi <arturminuss7@gmail.com>
2026-09-01 13:56:36 -0300

    Create ex3.py

---
+++ semana03/ex3.py
@@ -0,0 +1,11 @@
+resposta = input("Qual é a distância oficial de uma maratona?\n"
+                 "A) 21 km\n"
+                 "B) 42,195 km\n"
+                 "C) 10 km\n"
+                 "D) 5 km\n"
+                 "Digite a alternativa: ")
+
+if resposta.upper() == "B":
+    print("Acertou!")
+else:
+    print("Errou!")
