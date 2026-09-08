5ee668a006d01d9ffec57af97d20c65224dfcbdd
arturminussi <arturminuss7@gmail.com>
2026-09-08 18:19:25 -0300

    Create atvd5.py

---
+++ semana05/atvd5.py
@@ -0,0 +1,9 @@
+maiores = 0
+
+for i in range(10):
+    idade = int(input("Digite a idade: "))
+
+    if idade >= 18:
+        maiores = maiores + 1
+
+print("Quantidade de pessoas com 18 anos ou mais:", maiores)
